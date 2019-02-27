09d8ba813be9a776732117ae86e95aeacf9fe80c
github username <github email address>
2019-02-27 16:49:35 -0500

    validations added

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,7 +10,11 @@ class AuthorsController < ApplicationController
   end
   def create
     @author = Author.create(author_params)
-    redirect_to author_path(@author)
+    if @author.valid?
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
   def edit
     @author = Author.find(params[:id])
@@ -18,7 +22,11 @@ class AuthorsController < ApplicationController
   def update
     @author = Author.find(params[:id])
     @author.update(author_params)
-    redirect_to author_path(@author)
+    if @author.valid?
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
   def destroy
     @author = Author.find(params[:id])
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,10 @@
 class Author < ApplicationRecord
   has_many :book_authors
   has_many :books, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :birth_year, presence: true
+  validates :birth_year, numericality: { only_integer: true}
+  validates :birth_year, numericality: { less_than: 2020 }
 end
